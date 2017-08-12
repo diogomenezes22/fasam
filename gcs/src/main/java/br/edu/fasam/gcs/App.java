1d3a256f106ce2694f06e0eb1d16955046feff62
Aluísio Rodrigues Amaral <aluisioramtl@gmail.com>
2017-08-12 14:15:29 -0300

    Outro println

--- gcs/src/main/java/br/edu/fasam/gcs/App.java
+++ gcs/src/main/java/br/edu/fasam/gcs/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World FASAM222!" );
+        System.out.println( "Hello World 123!" );
     }
 }
