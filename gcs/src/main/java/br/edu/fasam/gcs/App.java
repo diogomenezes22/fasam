7ba3952354cad095a6776a722bee3a1d076a8650
diogomenezes22 <diogorochamenezes@gmail.com>
2017-08-12 13:58:30 -0300

    TESTE

--- gcs/src/main/java/br/edu/fasam/gcs/App.java
+++ gcs/src/main/java/br/edu/fasam/gcs/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World FASAM!" );
+        System.out.println( "Hello World FASAM2!" );
     }
 }
